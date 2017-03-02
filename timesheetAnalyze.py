1b89da5102737ad5f07be889b08f32041944c845
Andrii Kovalov <Andrii_Kovalov@jabil.com>
2017-03-02 16:57:34 +0200

    Окончание рефакторинга для верификации содержания файла

--- timesheetAnalyze.py
+++ timesheetAnalyze.py
@@ -6,8 +6,6 @@ import copy
 
 # Считываем список сотрудников из файла. Существует 2 типа написания имени и фамилии, мы раздилили их запятой.
 
-
-
 # Каталог из которого будем брать таймшиты
 # Home PC
 # directory = 'C:\\Users\\Andrey\\Desktop\\Timesheets 01-31\\'
@@ -17,17 +15,15 @@ filename = 'C:\\Users\\akovalyo\\Desktop\\employeesList.txt'
 directory = 'D:\\Reports\\For Bruce\\2017\\Timesheets 01-31\\'
 # directory = '\\\\KH-FSRV\\Public\\Timesheets\\2017\\02.28\\'
 
-
 file = open(filename, 'r')
 s = file.read()
 employees = re.split(r'; ', s)
 file.close()
 
-
 # Получаем список таймшитов в переменную timesheets
 timesheets = os.listdir(directory)
 
-# ****************************** Удаляем временные файлы из списка файлов считанных из директории ***********************************
+# ************** Удаляем временные файлы из списка файлов считанных из директории ********************
 # создать временную копию списка таймшита
 tmp_timesheets = copy.deepcopy(timesheets)
 
@@ -37,7 +33,6 @@ for i in tmp_timesheets:
         timesheets.remove(i)
 del tmp_timesheets
 
-#
 # ****************************** Проверяем все ли создали таймшиты ***********************************
 # Создаем копию списка с сотрудниками
 tmp_employees = copy.deepcopy(employees)
@@ -60,7 +55,6 @@ except Exception:
     print('==============================Exception========================================')
     print(e)
 
-
 # Выводим список людей, кто не создал таймшит
 print('     Готово ' + str(len(timesheets)) + ' таймшит(а). \nДолжно быть ' + str(len(employees)) + ' таймшит(а).')
 print('===============================================================================')
@@ -71,11 +65,10 @@ for i in range(0, int(len(tmp_employees))):
 
 del tmp_employees
 
-
 # ***************** Проверка праздничных дней, поля "Имя" и поля "Проект" ***********************************
 # Внесение праздничных выходных дней пользователем
 is_holiday = input("Есть ли выходные праздничные дни в этом месяце? (yes/no): ")
-holiday = [] # массив содержащий номера ячеек праздничных выходных
+holiday = []  # массив содержащий номера ячеек праздничных выходных
 if is_holiday == 'yes':
     sum_holiday_day = int(input("Сколько выходных дней в этот период?: "))
     # print(str(sum_holiday_day))
@@ -86,81 +79,35 @@ if is_holiday == 'yes':
 else:
     print("Нечего отдыхать! Труд сделал из обезьяны человека!")
 
-# ************************ Проверка  ************************************************
 print('===============================================================================')
-# wrong_timesheets = []
-# for i in timesheets:
-#     # print(i)
-#     try:
-#         # Считывваем имя сотрудника и название его первого проекта из таймшита
-#         f = OpenExcel(directory + i)
-#         username = f.read('C3').strip()
-#         project_1 = f.read('B8')
-#         # Считываем имя сотрудника из названия файла. Копируем строку до пробела и цифры.
-#         name_surname = re.search('(.*) \d', i)
-#         # Печатаем имя и фамилию сотрудников
-#         # print(name_surname.group(1))
-#         # Проверяем корректно ли заполнены поля Имя сотрудника и название проекта в таймшите
-#         if username != name_surname.group(1) or project_1 == 'Project1':
-#             wrong_timesheets.append(name_surname.group(1))
-#
-#         # Проверка, есть ли ошибка в файле в поле F4.
-#         error_message=f.read('F4')
-#         if len(error_message) > 0:
-#             print('В таймшите %s ошибка! Она выглядит вот так - %s' % (username, error_message))
-#             wrong_timesheets.append(name_surname.group(1))
-#
-#     except Exception:
-#         e = sys.exc_info()[1]
-#         # print(e.args[0])
-#         print('==============================Exception========================================')
-#         print(e)
-#
-# print('===============================================================================')
-# # Печатаем списсок людей с некорректным excel файлом
-# if len(wrong_timesheets) > 0:
-#     print('\nОшибка! Имя сотрудника или название проекта указано неверно.\nПнуть следующих людей:')
-#     for i in range(0, int(len(wrong_timesheets))):
-#         print(str(i + 1) + ') ' + wrong_timesheets[i])
-# print('===============================================================================')
-#
 
 for i in timesheets:
-        # Считывваем имя сотрудника и название его первого проекта из таймшита
-        f = OpenExcel(directory + i)    #         f = OpenExcel(directory + i)
-        username = f.read('C3').strip()
-        project_1 = f.read('B8')
-        error_message = f.read('F4')  # Ячейка F4 содержит ошибку, в случае если сумма часов за каждый рабочий день не равна 8
-        # Считываем имя сотрудника из названия файла. Копируем строку до пробела и цифры.
-        name_surname = re.search('(.*) \d', i)
+    # Считывваем имя сотрудника и название его первого проекта из таймшита
+    f = OpenExcel(directory + i)  # f = OpenExcel(directory + i)
+    username = f.read('C3').strip()
+    project_1 = f.read('B8')
+    # Ячейка F4 содержит ошибку, в случае если сумма часов за каждый рабочий день не равна 8
+    error_message = f.read('F4')
+    # Считываем имя сотрудника из названия файла. Копируем строку до пробела и цифры.
+    name_surname = re.search('(.*) \d', i)
 
-        # Проверка, есть ли ошибка в файле в поле F4.
-        if len(error_message) > 0:
-            print(f'В таймшите {name_surname.group(1)} ошибка! Она выглядит вот так - {error_message}')
-            # wrong_timesheets.append(name_surname.group(1))
+    # Проверка, есть ли ошибка в файле в поле F4.
+    if len(error_message) > 0:
+        print(f'В таймшите {name_surname.group(1)} ошибка! Она выглядит вот так - {error_message}')
 
-        # Проверяем корректно ли заполнены праздничные дни
-        if len(holiday) > 0:
-            for j in holiday:
-                holiday_day = f.read(j) # Проверка праздничных дней
-                if holiday_day != 8:
-                    print(f'В таймшите {name_surname.group(1)} ошибка! Часы для строки "Holiday" незаполнены.')
+    # Проверяем корректно ли заполнены праздничные дни
+    if len(holiday) > 0:
+        for j in holiday:
+            holiday_day = f.read(j)  # Проверка праздничных дней
+            if holiday_day != 8:
+                print(f'В таймшите {name_surname.group(1)} ошибка! Часы для строки "Holiday" незаполнены.')
 
-        # Проверяем корректно ли заполнены поля Имя сотрудника и название проекта в таймшите
-        if username != name_surname.group(1):
-            print('В таймшите %s ошибка в имени пользователя !' % name_surname.group(1))
+    # Проверяем корректно ли заполнены поля Имя сотрудника и название проекта в таймшите
+    if username != name_surname.group(1):
+        print('В таймшите %s ошибка в имени пользователя !' % name_surname.group(1))
 
-        if project_1 == 'Project1':
-            print('В таймшите %s ошибка в имени проекта !' % name_surname.group(1))
-
-        # elif holiday_day != 8:
-        #     print("Выходной не отмечен.")
-        #     # wrong_timesheets.append(name_surname.group(1))
+    if project_1 == 'Project1':
+        print('В таймшите %s ошибка в имени проекта !' % name_surname.group(1))
 
 print('===============================================================================')
-# # Печатаем списсок людей с некорректным excel файлом
-# if len(wrong_timesheets) > 0:
-#     print('\nОшибка! Имя сотрудника или название проекта указано неверно.\nПнуть следующих людей:')
-#     for i in range(0, int(len(wrong_timesheets))):
-#         print(str(i + 1) + ') ' + wrong_timesheets[i])
-# print('===============================================================================')
+
